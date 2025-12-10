70f59ec6bdee1528aa9307b8188e325f7639ec1b
VilemB <barnetv7@gmail.com>
2025-12-10 13:09:17 +0100

    removing test authentication

--- apps/user-application/src/router.tsx
+++ apps/user-application/src/router.tsx
@@ -29,16 +29,6 @@ export const trpcClient = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
       url: "/trpc",
-      // REMEMBER: reset before pushing (just for getting into the app locally)
-      headers: () => {
-        // Add bypass header for local development
-        if (import.meta.env.DEV) {
-          return {
-            "x-local-dev-bypass": "true",
-          };
-        }
-        return {};
-      },
     }),
   ],
 });
